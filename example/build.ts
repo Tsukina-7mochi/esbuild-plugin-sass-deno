fb7f63bbe376a377a26d470087303207468d5234
Tsukina-7mochi <tsukina7mochi@gmail.com>
2023-04-14 21:38:29 +0900

    fix example

--- example/build.ts
+++ example/build.ts
@@ -1,6 +1,6 @@
 import { esbuild } from "../deps.ts";
 import sassPlugin from "../mod.ts";
-import { readLines } from "https://deno.land/std@0.181.0/io/mod.ts";
+// import { readLines } from "https://deno.land/std@0.181.0/io/mod.ts";
 
 const ctx = await esbuild.context({
   entryPoints: [
