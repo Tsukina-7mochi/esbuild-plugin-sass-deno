9b4f343dd1b0ff8adf1bc8494670bd4a443a33a6
Tsukina-7mochi <Tsukina7mochi@gmail.com>
2024-06-07 00:22:47 +0900

    chore: update dependencies

--- deps.ts
+++ deps.ts
@@ -1,4 +1,4 @@
-export * as esbuild from "https://deno.land/x/esbuild@v0.19.2/mod.js";
-export * as fs from "https://deno.land/std@0.198.0/fs/mod.ts";
-export { posix } from "https://deno.land/std@0.198.0/path/mod.ts";
+export * as esbuild from "https://deno.land/x/esbuild@v0.21.4/mod.js";
+export * as fs from "https://deno.land/std@0.224.0/fs/mod.ts";
+export * as path from "https://deno.land/std@0.224.0/path/mod.ts";
 export { default as sass } from "https://deno.land/x/denosass@1.0.6/mod.ts";
